cc9d61a45b695366c8eba7b8a9ee768efa8fc953
dezxxx <37722460+dezxxx@users.noreply.github.com>
2026-03-30 18:44:07 +0300

    rename Main

--- src/com/dezxxx/javaDesignPatterns/factory/App.java
+++ src/com/dezxxx/javaDesignPatterns/factory/App.java
@@ -2,11 +2,11 @@ package com.dezxxx.javaDesignPatterns.factory;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class App {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 9; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
